6ba4f67f39560a08b927f807d9ef16e77587bb3a
maginawin <maginawin@maginawindeMac-mini.local>
2014-12-05 09:16:05 +0800

    Initial Commit

---
+++ LearnLearnLearn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LearnLearnLearn
+//
+//  Created by maginawin on 14-12-5.
+//  Copyright (c) 2014年 mycj.wwd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
